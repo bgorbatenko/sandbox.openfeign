6353ce689c79fbfc6775069b1e744492b6ce580e
Boris Gorbatenko <boris_gorbatenko@epam.com>
2022-05-09 23:43:47 +0300

    Logging of feign work

--- src/main/java/my/sandbox/feign/clientSide/SimpleFeign.java
+++ src/main/java/my/sandbox/feign/clientSide/SimpleFeign.java
@@ -1,6 +1,5 @@
 package my.sandbox.feign.clientSide;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/my/sandbox/feign/configuration/FeignConfiguration.java
+++ src/main/java/my/sandbox/feign/configuration/FeignConfiguration.java
@@ -1,9 +1,11 @@
 package my.sandbox.feign.configuration;
 
+import feign.Logger;
 import my.sandbox.feign.clientSide.SimpleFeign;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
@@ -11,4 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @RibbonClient(name = "restClient", configuration = {RibbonConfiguration.class})
 public class FeignConfiguration {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
 }
